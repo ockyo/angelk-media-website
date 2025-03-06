65e6f4fea1e9fe7829aaee090ad21b50f441874a
Ockyo <ockyo.0605@gmail.com>
2025-03-06 23:53:49 +0700

    update: news page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer';
 import Home from './pages/Home'
+import News from './pages/News/News';
 import CreatorAgency from './pages/CreatorAgency/CreatorAgency';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/creator-agency" element={<CreatorAgency />} />
+            <Route path="/thong-bao" element={<News />} />
           </Routes>
         </div>
         <Footer/>
